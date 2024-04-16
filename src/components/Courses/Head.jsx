0750652ad756266947d5bb2a255477a6b6840804
malja1500 <mianroudianf@gmail.com>
2024-04-16 13:19:23 +0330

    add courses Header

--- src/components/Courses/Head.jsx
+++ src/components/Courses/Head.jsx
@@ -2,7 +2,15 @@
 const Head = () => {
 
     return(
-        <div></div>
+        <div className=" w-[1232px] h-[484px]">
+            <img  src="/src/assets/images/Course/img.png"  className="absolute top-30 left-4 w-[484px] h-[385px]"></img>
+            <img  src="/src/assets/images/Course/Dots.png"  className=" absolute top-[198px] left-[608px] w-[126px] h-[154px]"></img>
+            <img src="/src/assets/images/Course/Dots2.png"  className="absolute top-[340px] right-0 w-[468px] h-[154px]"></img>
+            <img src="/src/assets/images/Course/Rectangle 25.png"  className="absolute top-[200px] right-[42px] w-[104px] h-[104px]"></img>
+            <p className="text-[24px] font-medium text-[#2196F3] absolute top-[240px] right-[90px]">مهمه از کی یاد می گیری!!</p>
+            <p className="text-[40px] font-medium absolute top-[284px] right-[90px]">اموزش برنامه نویسی با بهترین ها</p>
+            <p className="w-[528px] h-[50px] text-[16px] font-medium absolute top-[354px] right-[90px]">آموزش برنامه نویسی یکی از دوره‌های محبوب در حوزه فناوری اطلاعات است. برنامه نویسی مهارتی است که به افراد امکان می‌دهد تا نرم‌افزارهای کامپیوتری را ایجاد و توسعه دهند. </p>
+        </div>
 
     );
 
