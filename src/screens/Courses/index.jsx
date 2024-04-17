d932faa2d4156b3642c0946b5d825acf276dc243
Mohammad <bhshtbaran@gmail.com>
2024-04-17 07:26:19 -0700

    add the hero section of courses page

--- src/screens/Courses/index.jsx
+++ src/screens/Courses/index.jsx
@@ -1,11 +1,3 @@
-import { Up } from "../../components/Courses/coursesHeroSection";
-
-const CoursesHead = () => {
-  return <Up />;
-};
-
-export { CoursesHead };
-
 import { Courses } from "../../components/Courses";
 
 const CoursesPage = () => {
