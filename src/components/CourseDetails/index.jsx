562b8e94273acdbc4cb7606178586b397ab74238
Mohammad <bhshtbaran@gmail.com>
2024-04-18 12:21:46 -0700

    add course lessons and hour to the image box of course detail page

--- src/components/CourseDetails/index.jsx
+++ src/components/CourseDetails/index.jsx
@@ -4,13 +4,15 @@ import { priceWithCommas } from "../../core/utils/number-helper.utils";
 
 import { courseItems } from "../Courses/CourseItems/courseItems";
 import { CourseDetailsInformationBox } from "./CourseDetailsInformation/CourseDetailsInformationBox";
+import { CourseTeacher } from "./CourseDetailsInformation/CourseTeacher";
+import { CourseLikeBox } from "../common/CourseLikeBox";
 
 import studentsCountIcon from "../../assets/images/CourseDetails/Information/profile-user.svg";
 import courseStatusIcon from "../../assets/images/CourseDetails/Information/monitor-recorder.svg";
 import calenderIcon from "../../assets/images/CourseDetails/Information/calendar.svg";
 import calenderTickIcon from "../../assets/images/CourseDetails/Information/calendar-tick.svg";
-import { CourseTeacher } from "./CourseDetailsInformation/CourseTeacher";
-import { CourseLikeBox } from "../common/CourseLikeBox";
+import noteIcon from "../../assets/images/CourseDetails/Icons/note.svg";
+import clockIcon from "../../assets/images/CourseDetails/Icons/clock.svg";
 
 const CourseDetails = () => {
   const { id } = useParams();
@@ -25,8 +27,17 @@ const CourseDetails = () => {
         <div className="relative">
           <img src={course.image} className="rounded-[24px]" />
           <CourseLikeBox classes="absolute top-10 right-8 bg-white" />
-          <div className="flex">
-            <div></div>
+          <div className="flex gap-4 absolute bottom-5 left-5">
+            <div className="flex gap-1 justify-center items-center bg-white w-[119px] h-[48px] rounded-[50px]">
+              <img src={noteIcon} className="-mt-[3px]" />
+              <span className="font-[500] text-text1">
+                {course.lessonsCount} درس
+              </span>
+            </div>
+            <div className="flex gap-1 justify-center items-center bg-white w-[119px] h-[48px] rounded-[50px]">
+              <img src={clockIcon} className="-mt-[3px]" />
+              <span className="font-[500] text-text1">{course.hour} ساعت</span>
+            </div>
             <div></div>
           </div>
         </div>
