3d12a28bd22c3498dcb925489636f8b6d1f2411e
Mohammad <bhshtbaran@gmail.com>
2024-04-18 00:59:00 -0700

    add courses

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,3 +1,4 @@
+import { CourseItemsMapped } from "./CourseItems/CourseItemsMapped";
 import { Filters } from "./CoursesFilter/Filters";
 import { FilterTitleTrash } from "./CoursesFilter/FilterTitleTrash";
 import { CoursesHeroSection } from "./CoursesHeroSection";
@@ -6,8 +7,8 @@ const Courses = () => {
   return (
     <>
       <CoursesHeroSection />
-      <div className="flex gap-5 w-[1050px] mx-auto mt-32 px-5 lg:px-0">
-        <div className="w-[296px] rounded-[24px] shadow-primaryShadow py-4 bg-white">
+      <div className="flex justify-center gap-5 w-[90%] mx-auto mt-32 px-5 lg:px-0">
+        <div className="w-[296px] rounded-[24px] shadow-primaryShadow py-4 bg-white mt-10">
           <div className="px-2">
             <FilterTitleTrash />
           </div>
@@ -15,7 +16,9 @@ const Courses = () => {
             <Filters />
           </div>
         </div>
-        <div></div>
+        <div className="w-[79%] flex gap-x-3 flex-wrap">
+          <CourseItemsMapped />
+        </div>
       </div>
     </>
   );
