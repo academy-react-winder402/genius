b2f3cad9ce83a48e74eda50cb44cb927c0be612c
Mohammad <bhshtbaran@gmail.com>
2024-04-22 10:55:17 -0700

    create store and add Provider to the main

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
-import { router } from "./config/router";
 import { theme } from "./config";
+import { router } from "./config/router";
+import { store } from "./redux/store";
 
 import { ThemeProvider } from "@mui/material";
 
-import "./index.css";
 import "./assets/css/fonts.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: Posts, comments: CommentsState, users: UserState  }
+export type AppDispatch = typeof store.dispatch;
