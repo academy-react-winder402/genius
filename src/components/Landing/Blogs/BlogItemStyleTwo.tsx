d288ab31a76b4dbc8b0b506df27a032af1c1385f
Mohammad <bhshtbaran@gmail.com>
2024-04-23 07:43:21 -0700

    apply dark mode classes to the BlogItemStyleTwo of landing landing page

--- src/components/Landing/Blogs/BlogItemStyleTwo.tsx
+++ src/components/Landing/Blogs/BlogItemStyleTwo.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { BlogItemInterface } from "../../../types/blog-items";
+
+import { useDarkModeSelector } from "../../../redux/darkMode";
 
 import { Link } from "../../common/Link";
 
 import calenderIcon from "../../../assets/images/Landing/LandingBlogs/Icons/calendar.svg";
 import eyeIcon from "../../../assets/images/Landing/LandingBlogs/Icons/eye.svg";
-import { BlogItemInterface } from "../../../types/blog-items";
+import calenderDarkIcon from "../../../assets/images/Landing/LandingBlogs/Icons/calendar-dark.svg";
+import eyeDarkIcon from "../../../assets/images/Landing/LandingBlogs/Icons/eye-dark.svg";
 
 const BlogItemStyleTwo = ({
   id,
@@ -14,6 +17,8 @@ const BlogItemStyleTwo = ({
   title,
   description,
 }: BlogItemInterface) => {
+  const darkMode = useDarkModeSelector();
+
   return (
     <div className="flex flex-col gap-4 px-4">
       <Link to={`/blogs/${id}`}>
@@ -21,20 +26,28 @@ const BlogItemStyleTwo = ({
       </Link>
       <div className="flex gap-4 mt-4">
         <div className="landingBlogItemStyleTwoBackground">
-          <img src={eyeIcon} className="-mt-[2px]" />
+          <img
+            src={darkMode === true ? eyeDarkIcon : eyeIcon}
+            className="-mt-[2px]"
+          />
           <span className="landingBlogItemsStyleTwoColor">
             {allSeas} بازدید
           </span>
         </div>
         <div className="landingBlogItemStyleTwoBackground">
-          <img src={calenderIcon} className="-mt-[2px]" />
+          <img
+            src={darkMode ? calenderDarkIcon : calenderIcon}
+            className="-mt-[2px]"
+          />
           <span className="landingBlogItemsStyleTwoColor">{createdAt}</span>
         </div>
       </div>
-      <h3 className="font-[700] text-[20px] lg:text-[32px] text-text1 cursor-pointer">
+      <h3 className="font-[700] text-[20px] lg:text-[32px] text-text1 dark:text-darkText cursor-pointer">
         <Link to={`/blogs/${id}`}>{title}</Link>
       </h3>
-      <p className="font-[500] text-text2 text-justify">{description}</p>
+      <p className="font-[500] text-text2 dark:text-darkText text-justify">
+        {description}
+      </p>
     </div>
   );
 };
