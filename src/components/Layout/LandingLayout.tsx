5f6092782284809422c54028c6e4ab8be3218552
Mohammad <bhshtbaran@gmail.com>
2024-04-23 01:15:48 -0700

    move mainLayout and landingLayout to the Layout folder

--- src/components/Layout/LandingLayout.tsx
+++ src/components/Layout/LandingLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
 
 const LandingLayout = () => {
   return (
