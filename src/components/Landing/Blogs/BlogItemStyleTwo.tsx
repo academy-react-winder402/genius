4830bf2185e29c9f84d19e0b3a2aeb5e8b4a9293
Mohammad <bhshtbaran@gmail.com>
2024-04-23 07:54:30 -0700

    fix BlogItemStyleTwo dark mode classed of landing page

--- src/components/Landing/Blogs/BlogItemStyleTwo.tsx
+++ src/components/Landing/Blogs/BlogItemStyleTwo.tsx
@@ -1,13 +1,9 @@
 import { BlogItemInterface } from "../../../types/blog-items";
 
-import { useDarkModeSelector } from "../../../redux/darkMode";
-
 import { Link } from "../../common/Link";
 
 import calenderIcon from "../../../assets/images/Landing/LandingBlogs/Icons/calendar.svg";
 import eyeIcon from "../../../assets/images/Landing/LandingBlogs/Icons/eye.svg";
-import calenderDarkIcon from "../../../assets/images/Landing/LandingBlogs/Icons/calendar-dark.svg";
-import eyeDarkIcon from "../../../assets/images/Landing/LandingBlogs/Icons/eye-dark.svg";
 
 const BlogItemStyleTwo = ({
   id,
@@ -17,8 +13,6 @@ const BlogItemStyleTwo = ({
   title,
   description,
 }: BlogItemInterface) => {
-  const darkMode = useDarkModeSelector();
-
   return (
     <div className="flex flex-col gap-4 px-4">
       <Link to={`/blogs/${id}`}>
@@ -26,19 +20,13 @@ const BlogItemStyleTwo = ({
       </Link>
       <div className="flex gap-4 mt-4">
         <div className="landingBlogItemStyleTwoBackground">
-          <img
-            src={darkMode === true ? eyeDarkIcon : eyeIcon}
-            className="-mt-[2px]"
-          />
+          <img src={eyeIcon} className="-mt-[2px]" />
           <span className="landingBlogItemsStyleTwoColor">
             {allSeas} بازدید
           </span>
         </div>
         <div className="landingBlogItemStyleTwoBackground">
-          <img
-            src={darkMode ? calenderDarkIcon : calenderIcon}
-            className="-mt-[2px]"
-          />
+          <img src={calenderIcon} className="-mt-[2px]" />
           <span className="landingBlogItemsStyleTwoColor">{createdAt}</span>
         </div>
       </div>
