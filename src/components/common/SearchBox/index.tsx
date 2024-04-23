87edb518cd0513038b8344234823b8defae321a8
Mohammad <bhshtbaran@gmail.com>
2024-04-23 05:40:40 -0700

    fix landing search box in landing page

--- src/components/common/SearchBox/index.tsx
+++ src/components/common/SearchBox/index.tsx
@@ -6,6 +6,7 @@ interface SearchBoxProps {
   inputClasses?: string;
   isMarginTop?: boolean;
   display?: string;
+  isLanding?: boolean;
 }
 
 const SearchBox = ({
@@ -13,13 +14,16 @@ const SearchBox = ({
   inputClasses,
   isMarginTop,
   display,
+  isLanding,
 }: SearchBoxProps) => {
   return (
     <div className={`${display} ${isMarginTop ? "mt-10" : ""}`}>
       <div className="relative w-[100%] lg:w-auto flex justify-center">
         <input
           placeholder={placeholder}
-          className={`outline-none px-3 pl-12 rounded-[8px] placeholder:text-text-2 dark:text-darkText w-[97%] h-[50px] ${inputClasses} dark:bg-gray-900`}
+          className={`outline-none px-3 pl-12 rounded-[8px] placeholder:text-text-2 dark:text-darkText w-[97%] h-[50px] ${inputClasses} ${
+            isLanding === true ? "dark:!bg-gray-800" : "dark:bg-gray-900"
+          }`}
         />
         <img src={searchIcon} className="absolute left-6 lg:left-4 top-3" />
       </div>
