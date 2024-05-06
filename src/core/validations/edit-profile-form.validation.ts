a2dee263368fb6a432c7903dc935f683621eff4c
malja1500 <mianroudianf@gmail.com>
2024-05-06 23:52:53 +0330

    fix some code

--- src/core/validations/edit-profile-form.validation.ts
+++ src/core/validations/edit-profile-form.validation.ts
@@ -1,10 +1,10 @@
 import * as yup from "yup";
 
 export const editProfileFormSchema = yup.object().shape({
-  firstName: yup.string().required("این فیلد الزامی می باشد"),
-  lastName: yup.string().required("این فیلد الزامی می باشد"),
-  nationalCode: yup.string().required("این فیلد الزامی می باشد"),
-  email: yup.string().required("این فیلد الزامی می باشد"),
-  birthdayDate: yup.string().required("این فیلد الزامی می باشد"),
-  phoneNumber: yup.string().required("این فیلد الزامی می باشد"),
+  firstName: yup.string().required("لطفا نام خود را وارد کنید"),
+  lastName: yup.string().required("لطفا نام خانوادگی خود را وارد کنید"),
+  nationalCode: yup.string().required("لطفا کد ملی خود را وارد کنید"),
+  email: yup.string().required("لطفا ایمیل خود را وارد کنید"),
+  birthdayDate: yup.string().required("لطفا تاریخ تولد خود را وارد کنید"),
+  phoneNumber: yup.string().required("لطفا شماره موبایل خود را وارد کنید"),
 });
