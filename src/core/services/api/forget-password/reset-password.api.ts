d047cd2953c6fcf77490b445660031cbeb61cd62
Mohammad <bhshtbaran@gmail.com>
2024-05-09 06:56:56 -0700

    add reset password api for forget password

---
+++ src/core/services/api/forget-password/reset-password.api.ts
@@ -0,0 +1,19 @@
+import http from "../../interceptor";
+
+export const resetPasswordAPI = async (
+  userId: number,
+  newPassword: string,
+  resetValue: string
+) => {
+  try {
+    const response = http.post("/Sign/Reset", {
+      userId,
+      newPassword,
+      resetValue,
+    });
+
+    return response;
+  } catch (error) {
+    return false;
+  }
+};
