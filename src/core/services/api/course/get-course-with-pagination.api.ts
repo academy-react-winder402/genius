47a45776e40d8bd35e0d69fca022fa8a2b417357
Mohammad <bhshtbaran@gmail.com>
2024-05-19 12:25:36 -0700

    implement get course with pagination api

---
+++ src/core/services/api/course/get-course-with-pagination.api.ts
@@ -0,0 +1,43 @@
+import http from "../../interceptor";
+
+export const getCourseWithPaginationAPI = async (
+  pageNumber: number | undefined,
+  rowsOfPage: number | undefined,
+  sortingCol: string | undefined,
+  sortType: string | undefined,
+  query: string | undefined,
+  costDown: number | undefined,
+  costUp: number | undefined,
+  techCount: number | undefined,
+  listTech: string | undefined,
+  courseLevelId: number | undefined,
+  courseTypeId: number | undefined,
+  startDate: Date | undefined,
+  endDate: Date | undefined,
+  teacherId: number | undefined
+) => {
+  try {
+    const response = http.get("/Home/GetCoursesWithPagination", {
+      params: {
+        pageNumber,
+        rowsOfPage,
+        sortingCol,
+        sortType,
+        query,
+        costDown,
+        costUp,
+        techCount,
+        listTech,
+        courseLevelId,
+        courseTypeId,
+        startDate,
+        endDate,
+        teacherId,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    return false;
+  }
+};
