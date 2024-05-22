c6a42c48abe87a2b0f8ee73b3f0cf52d1010ea4d
Mohammad <bhshtbaran@gmail.com>
2024-05-22 10:04:17 -0700

    add content type in header of add comment course api

--- src/core/services/api/course/comments/add-comment-course.api.ts
+++ src/core/services/api/course/comments/add-comment-course.api.ts
@@ -2,7 +2,11 @@ import http from "../../../interceptor";
 
 export const addCommentCourseAPI = async (comment: FormData) => {
   try {
-    const response = await http.post("/Course/AddCommentCourse", comment);
+    const response = await http.post("/Course/AddCommentCourse", comment, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
 
     return response;
   } catch (error) {
