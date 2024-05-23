2a56ef7641f055f61aef324475ecf894fff6c9c7
Mohammad <bhshtbaran@gmail.com>
2024-05-23 07:17:26 -0700

    install and initialize react query and react query devtools packages

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,9 +3,12 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "../redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { getItem } from "../core/services/common/storage.services";
+
+import { persistor, store } from "../redux/store";
 
 import { theme } from "../config";
 import { authRoutes } from "../config/router/auth";
@@ -14,17 +17,22 @@ import { publicRoutes } from "../config/router/public.router";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   const isLoggedIn = getItem("token");
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-          <RouterProvider router={isLoggedIn ? authRoutes : publicRoutes} />
-          <ToastContainer rtl />
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <RouterProvider router={isLoggedIn ? authRoutes : publicRoutes} />
+            <ToastContainer rtl />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
